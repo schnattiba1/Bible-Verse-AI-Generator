b1939f7cebdd2d42cd958e7079bf798524a214b0
Chanel Chinedu <LongLiveTheKing777@protonmail.com>
2026-01-30 12:19:45 +0000

    Added theme preference and media responsiveness

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
+// Load saved theme preference from local storage
+function loadWindow() {
+  let savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.querySelector("body").classList.add("dark-theme");
+    document.querySelector("#theme-toggle img").src = "./imgs/light-mode.png";
+  }
+}
+
+// Display dark or light theme
+function displayTheme() {
+  let bodyElement = document.querySelector("body");
+  bodyElement.classList.toggle("dark-theme");
+  let themeToggle = document.querySelector("#theme-toggle img");
+
+  if (bodyElement.classList.contains("dark-theme")) {
+    themeToggle.src = "./imgs/light-mode.png";
+  } else {
+    themeToggle.src = "./imgs/night-mode.png";
+  }
+  // Save theme preference into local storage
+  if (bodyElement.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// Display generated Bible verse with typewriter effect
 function displayVerse(response) {
   new Typewriter("#generated-answer", {
     strings: response.data.answer,
@@ -13,7 +42,7 @@ function generateVerse(event) {
   let inputTextElement = document.querySelector("#input-text");
 
   let apiKey = "7b83at211fa42375b047407234bfo5f1";
-  let prompt = `Generate a short Bible verse based on the user's emotions: ${inputTextElement.value}, i.e, happy, upset, sad, excited, etc. Only respond with Bible verses and nothing else. If the user types random letters, i.e, sghjjskldf etc response with: "Please provide a valid emotion so I can generate a Bible verse for you."`;
+  let prompt = `Generate a short Bible verse based on the user's emotions: ${inputTextElement.value}, i.e, happy, upset, sad, excited, etc. If the user types random letters, i.e, sghjjskldf etc response with: 'Please provide a valid emotion so I can generate a Bible verse for you.' Make sure you generate a different bible verse everytime the user enters an emotion.`;
   let context =
     "Make sure you provide the correct book, chapter, and verse number for each Bible verse you generate and especially based on the user's emotions. Respond with only the Bible verse and nothing else. Make sure is written to the end.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
@@ -21,5 +50,13 @@ function generateVerse(event) {
   axios.get(apiUrl).then(displayVerse);
 }
 
+// Event listener for generate button
 let btnElement = document.querySelector("#btn");
 btnElement.addEventListener("click", generateVerse);
+
+// Dark mode toggle
+let themeToggle = document.querySelector("#theme-toggle");
+themeToggle.addEventListener("click", displayTheme);
+
+// Load theme preference from local storage
+window.addEventListener("load", loadWindow);
